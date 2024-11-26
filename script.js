b7d7584674a528f423b2dd6d7f4f086c4bf2ae6d
Anna shabbagh <afreenshabbagh@gmail.com>
2024-11-26 12:41:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 //complete this code
-class Animal {}
+// Define the Animal class
+class Animal {
+    constructor(species) {
+        this._species = species;
+    }
 
-class Dog extends Animal {}
+    // Getter for species
+    get species() {
+        return this._species;
+    }
 
-class Cat extends Animal {}
+    // Method to make a sound
+    makeSound() {
+        console.log(`The ${this.species} makes a sound`);
+    }
+}
+
+// Define the Cat class that extends Animal
+class Cat extends Animal {
+    // Method for purring
+    purr() {
+        console.log("purr");
+    }
+}
+
+// Define the Dog class that extends Animal
+class Dog extends Animal {
+    // Method for barking
+    bark() {
+        console.log("woof");
+    }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
